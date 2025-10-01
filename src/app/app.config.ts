e04b6d79d9249a07958c963cd94b1168576cbc3a
Victoria Rodovalho <victorialicia22@gmail.com>
2025-10-01 18:41:22 -0300

    adicionando PrimeNG ao projeto e estilos iniciais

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura // 👈 aqui você escolhe Aura, Lara, Nora…
+      }
+    })
+  ]
 };
