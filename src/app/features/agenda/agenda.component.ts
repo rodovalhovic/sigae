a7399667132e2837f603981952ac6fa74aed726c
Victoria Rodovalho <victorialicia22@gmail.com>
2025-10-07 08:06:27 -0300

    ajustes na agenda para exibir na home

--- src/app/features/agenda/agenda.component.ts
+++ src/app/features/agenda/agenda.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions } from '@fullcalendar/core';
+import { CalendarOptions, EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { DialogModule } from 'primeng/dialog';
+import { AgendaService } from '../../core/agenda.service';
 
 type Extra = { assunto: string; participantes: string[]; descricao: string };
 
@@ -21,6 +22,28 @@ export class AgendaComponent {
   visible = false;
   selected: (Extra & { title: string }) | null = null;
 
+  private eventos: EventInput[] = [
+    {
+      title: 'Reunião de Alinhamento',
+      start: '2025-10-10T10:00:00',
+      end: '2025-10-10T11:00:00',
+      extendedProps: {
+        assunto: 'Reunião',
+        participantes: ['Victoria', 'Thiago'],
+        descricao: 'Pauta: próximos passos do plano de melhoria.'
+      } as Extra
+    },
+    {
+      title: 'Apresentação de Resultados',
+      start: '2025-10-12T15:00:00',
+      extendedProps: {
+        assunto: 'Apresentação',
+        participantes: ['Equipe TI', 'Gestores'],
+        descricao: 'Apresentar métricas do SIGAE.'
+      } as Extra
+    }
+  ];
+
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
@@ -29,33 +52,18 @@ export class AgendaComponent {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
-    events: [
-      {
-        title: 'Reunião de Alinhamento',
-        start: '2025-10-10T10:00:00',
-        end: '2025-10-10T11:00:00',
-        extendedProps: {
-          assunto: 'Reunião',
-          participantes: ['Victoria', 'Thiago'],
-          descricao: 'Pauta: próximos passos do plano de melhoria.'
-        } as Extra
-      },
-      {
-        title: 'Apresentação de Resultados',
-        start: '2025-10-12T15:00:00',
-        extendedProps: {
-          assunto: 'Apresentação',
-          participantes: ['Equipe TI', 'Gestores'],
-          descricao: 'Apresentar métricas do SIGAE.'
-        } as Extra
-      }
-    ],
+    events: this.eventos,
     eventClick: (info) => {
       const extra = info.event.extendedProps as Extra;
       this.selected = { title: info.event.title, ...extra };
       this.visible = true;
     }
   };
+
+  constructor(private agendaService: AgendaService) {
+    // publica os eventos para que a Home possa contar
+    this.agendaService.setAll(this.eventos);
+  }
 
   closeDialog() {
     this.visible = false;
