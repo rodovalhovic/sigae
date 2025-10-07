22b465ab400f9ab22b68fa01aa42bff567484d32
Victoria Rodovalho <victorialicia22@gmail.com>
2025-10-07 00:00:04 -0300

    adicionando fullCalendar para fazer a agenda

--- src/app/features/agenda/agenda.component.ts
+++ src/app/features/agenda/agenda.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { DialogModule } from 'primeng/dialog';
+
+type Extra = { assunto: string; participantes: string[]; descricao: string };
 
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FullCalendarModule, DialogModule],
   templateUrl: './agenda.component.html',
-  styleUrl: './agenda.component.scss'
+  styleUrls: ['./agenda.component.scss']
 })
 export class AgendaComponent {
+  visible = false;
+  selected: (Extra & { title: string }) | null = null;
 
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    events: [
+      {
+        title: 'Reunião de Alinhamento',
+        start: '2025-10-10T10:00:00',
+        end: '2025-10-10T11:00:00',
+        extendedProps: {
+          assunto: 'Reunião',
+          participantes: ['Victoria', 'Thiago'],
+          descricao: 'Pauta: próximos passos do plano de melhoria.'
+        } as Extra
+      },
+      {
+        title: 'Apresentação de Resultados',
+        start: '2025-10-12T15:00:00',
+        extendedProps: {
+          assunto: 'Apresentação',
+          participantes: ['Equipe TI', 'Gestores'],
+          descricao: 'Apresentar métricas do SIGAE.'
+        } as Extra
+      }
+    ],
+    eventClick: (info) => {
+      const extra = info.event.extendedProps as Extra;
+      this.selected = { title: info.event.title, ...extra };
+      this.visible = true;
+    }
+  };
+
+  closeDialog() {
+    this.visible = false;
+    this.selected = null;
+  }
 }
